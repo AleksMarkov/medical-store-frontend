288a9564f32bf5111504745e2460727da12cb549
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-08-27 11:52:31 -0400

    updated order and added the svg for filter, slider

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import PublicRoute from "./PublicRoute";
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 const SharedLayout = lazy(() => import("./pages/SharedLayout/SharedLayout")); // Новый SharedLayout компонент
 const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"));
-const Orders = lazy(() => import("./pages/Orders/Orders"));
+const Orders = lazy(() => import("./components/Orders/Orders"));
 const Products = lazy(() => import("./pages/Products/Products"));
 const Suppliers = lazy(() => import("./pages/Suppliers/Suppliers"));
 const Customers = lazy(() => import("./pages/Customers/Customers"));
